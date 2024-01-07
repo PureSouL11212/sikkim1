e611f55b6396d0b1cbc36fe01eb743a2470b2fce
ITS_A_PURE_SOUL <109498451+PureSouL11212@users.noreply.github.com>
2024-01-07 17:20:22 +0530

    rett

--- src/app/secondnavbar/secondnavbar.component.ts
+++ src/app/secondnavbar/secondnavbar.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-secondnavbar',
   templateUrl: './secondnavbar.component.html',
   styleUrls: ['./secondnavbar.component.css']
 })
-export class SecondnavbarComponent {
-  showDropdown = false;
-  selectService(service: string) {
-    // Add logic here to handle the selected service
-    console.log(`Selected service: ${service}`);
+export class SecondnavbarComponent implements OnInit {
+  constructor(){}
+  ngOnInit(): void {
   }
+  
 }
